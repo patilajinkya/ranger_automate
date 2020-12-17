7817110b65796280c46fb9753c78d4028d64abe3
patilajinkya <35138923+patilajinkya@users.noreply.github.com>
2020-12-16 20:54:08 -0600

    Update roles_exporter.py

--- roles_exporter.py
+++ roles_exporter.py
@@ -11,7 +11,7 @@ from getpass import getpass
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-IMPORT_RANGER_URL = raw_input("SOURCE_ENTER RANGER URL:- ")
+IMPORT_RANGER_URL = raw_input("SOURCE_RANGER URL:- ")
 ROLES_API = "/service/roles/roles/"
 IMPORT_RANGER_ADMIN_USER = raw_input("SOURCE_RANGER ADMIN USER:- ")
 IMPORT_RANGER_ADMIN_PASSWORD = getpass(prompt='SOURCE_RANGER ADMIN PASSWORD:- ', stream=None)
@@ -28,7 +28,7 @@ TOTAL_ROLES = len(ROLES)
 
 print "Total number of roles " + str(TOTAL_ROLES) + " will be exported."
 
-EXPORT_RANGER_URL = raw_input("DEST_ENTER RANGER URL:- ")
+EXPORT_RANGER_URL = raw_input("DEST_RANGER URL:- ")
 EXPORT_RANGER_ADMIN_USER = raw_input("DEST_RANGER ADMIN USER:- ")
 EXPORT_RANGER_ADMIN_PASSWORD = getpass(prompt='DEST_RANGER ADMIN PASSWORD:- ', stream=None)
 headers = {'Accept' : 'application/json'}
@@ -46,6 +46,15 @@ for ROLE in ROLES :
     if STATUS is 200:
         print "Importing role" + ROLENAME + " with status " + str(STATUS)
         SUCCESS_ROLES.append(ROLENAME)
+        IMPORTED_ROLES.append(ROLENAME)
     else:
         print "Import for role " + ROLENAME + " failed with status " + str(STATUS)
         FAILED_ROLES.append(ROLENAME)
+        FAILED_ROLES.append(ROLENAME)
+        
+print "\n" + str(len(FAILED_ROLES)) + " Roles have failed to export" + \
+      "\n" + str(len(IMPORTED_ROLES)) + " Roles have been imported successfully"
+
+print "\nCould not import Following roles:- "
+for FAILED_ROLE in FAILED_ROLES:
+    print FAILED_ROLE
